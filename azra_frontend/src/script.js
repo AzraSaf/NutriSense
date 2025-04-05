9debefd26f430388c2d914db9ff80f32165a29e7
SaltedTacos <165761546+SaltedTacos@users.noreply.github.com>
2025-04-05 20:33:01 +0530

    Deleted the unneccesary files to show the only neccesary file to run the application

--- azra_frontend/src/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        alert("Thank you for reaching out! We will get back to you soon.");
-        form.reset();
-    });
-});
